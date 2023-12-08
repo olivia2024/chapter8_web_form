bfd8f4f73b7f9983716191298f1fc95d1021c172
olivia2024 <theoliviatales@gmail.com>
2023-12-08 11:19:45 +0300

    Initial commit

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 
 class Loginform(FlaskForm):
     """Login Form"""
     username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
+    email = StringField('Email Addess', validators=[DataRequired(), Length(1, 64)])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+class Productform(FlaskForm):
+    """Add Product Form"""
+    product_name = StringField('Product Name', validators=[DataRequired(), Length(1, 64)])
+    product_description = TextAreaField('Product Description', validators=[DataRequired()])
+    stock_available = SelectField('Stock Available', validators=[DataRequired()], 
+                                  choices=[('1', '1'), ('4', '4'), ('8', '8')])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,27 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
-from app.forms import Loginform
+from app.forms import Loginform, Productform
 @app.route('/')
 @app.route('/home')
 def index():
     """Index URL"""
     return render_template('index.html', title='Index Page')
 
-@app.route('/about_me')
-def about_me():
-    """About Me URL"""
-    return render_template('about_me.html', title='About Me Page')
+@app.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+    """Add Product URL"""
+    form = Productform()
+    if form.validate_on_submit():
+        flash(f'You are requesting to add a product {form.product_name.data}')
+        return redirect(url_for('index'))
+    return render_template('add_product.html', title='Add Product', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    """Login URL"""
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register URL"""
     form = Loginform()
     if form.validate_on_submit():
-        flash(f'You are requesting to login {form.username.data}')
+        flash(f'You are requesting to register {form.username.data}')
         return redirect(url_for('index'))
-    return render_template('login.html', title='Login', form=form)
+    return render_template('register.html', title='Register', form=form)
 
